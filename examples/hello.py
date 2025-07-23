c3394ce98c1802c109ddc48c257e68eff44ae138
ThisIsAUniqueUserName123 <brownlucas222+dataannotations@gmail.com>
2025-07-23 11:13:56 -0400

    I don't want tests lol

--- examples/hello.py
+++ examples/hello.py
@@ -1,3 +1,21 @@
-print("Hello!")
+import numpy as np
+from src.LinearOperatorSystem import LinearOperatorSystem
+from src.BoundaryConditions import zero_boundary_condition 
 
-from src import LinearOperatorSystem
+# Define the coordinate grid 
+cartesian_divisions = [1000]
+cartesian_bounds = [(-5, 5)]  # Domain boundaries
+
+# Build the cartesian system 
+cartesian_system = LinearOperatorSystem(
+    bounds=cartesian_bounds,
+    num_divisions=cartesian_divisions,
+    operator_dict={
+        (2,): lambda x: -np.ones_like(x)/2,
+    },
+    boundary_conditions=[zero_boundary_condition(0)]
+)
+
+# Find the ground state solution
+_, psi_cartesian = cartesian_system.compute_eigenstates(1)
+cartesian_system.plot_eigenstates(psi_cartesian, 1, complex_tool=lambda x: np.abs(x)**2)
